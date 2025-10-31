99e239bf88cff34d658a474a9a189d2b70c93dd0
SylviaEmily <163227303+SylviaEmily@users.noreply.github.com>
2025-10-31 09:37:02 +0700

    push front end

--- backend/urls.py
+++ backend/urls.py
@@ -1,29 +1,26 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# backend/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin & API
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls')),  # hapus baris ini kalau tidak punya app api
+
+    # ===== PAGES (no .html) =====
+    path('',             TemplateView.as_view(template_name='circleup/home.html'),        name='home'),
+    path('explore/',     TemplateView.as_view(template_name='circleup/explore.html'),     name='explore'),
+    path('chat/',        TemplateView.as_view(template_name='circleup/chat.html'),        name='chat'),
+    path('feeds/',       TemplateView.as_view(template_name='circleup/feeds.html'),       name='feeds'),
+    path('event-info/',  TemplateView.as_view(template_name='circleup/event-info.html'),  name='event_info'),
+    path('members/',     TemplateView.as_view(template_name='circleup/members.html'),     name='members'),
+    path('about/',       TemplateView.as_view(template_name='circleup/about.html'),       name='about'),
 ]
 
+# Static/media saat development (aman dibiarkan)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
